fixing sigmoid/step gradient bug -- seems to fix problems with unfreezing attn module

char_seq_to_masked_emb/experiment.py
@@ -340,7 +340,7 @@ class StepWithSigmoidGrad(torch.autograd.Function):
     @staticmethod
     def backward(self,grad_output):
         sig = torch.nn.Sigmoid()(self.x)
-        return sig*(1-sig)
+        return sig*(1-sig)*grad_output
 
 class StepWithHardGrad(torch.autograd.Function):
     @staticmethod
@@ -353,7 +353,7 @@ class StepWithHardGrad(torch.autograd.Function):
         grad_input = grad_output.clone()
         grad_input[self.high] = 1
         grad_input[~self.high] = 0
-        return grad_input
+        return grad_input*grad_output
 
 def hard_sigmoid(x):
   return x.clamp(min=0.0, max=1.0)
@@ -612,7 +612,7 @@ class CharEmbedder(nn.Module):
 
       # TODO clean and include other special chars.
       act[spaces[0], torch.clamp(spaces[1]+1, max=act.shape[-1]-1)] = 1
-      special_chars = torch.where((x<32) & (x!=1))
+      special_chars = torch.where((x<=32) & (x!=1))
       act[special_chars] = 1
       act[torch.where(act==0)]=-1
       act[:, 0] = 1

token_start_pred/experiment.py
@@ -116,8 +116,18 @@ class TokenStartDataset(torch.utils.data.Dataset):
   """
   def __init__(self, data_file, device='cuda'):
     self.data = torch.load(data_file)
-    self.data['char_encoded_seqs'] = self.data['chars_encoded'].to(device).long()
-    self.data['token_start_positions'] = self.data['token_start_offsets'].to(device).float()
+    self.data['char_encoded_seqs'] = self.data['chars_encoded'].to(device)
+    # if the dataset has start and end indexes - convert to idx 1-marked.
+    if len(self.data['token_start_offsets'].shape) == 3:
+      starts = self.data['token_start_offsets'][:, :, 0]
+      seq_enc_starts = torch.zeros(self.data['char_encoded_seqs'].shape).to(starts.device)
+      seq_enc_starts[torch.arange(self.data['char_encoded_seqs'].shape[0]).view(-1,1), starts.long()] = 1
+      self.data['token_start_positions'] = seq_enc_starts.to(device)
+      print("2part offset")
+    else:
+      self.data['token_start_positions'] = self.data['token_start_offsets']
+      print("1hot encoding ish")
+      
     assert len(self.data['char_encoded_seqs']) == len(self.data['token_start_positions'])
     
   def __len__(self):
@@ -125,8 +135,8 @@ class TokenStartDataset(torch.utils.data.Dataset):
 
   def __getitem__(self, idx):
     return {
-      'char_encoded_seqs': self.data['char_encoded_seqs'][idx],
-      'token_start_positions': self.data['token_start_positions'][idx]
+      'char_encoded_seqs': self.data['char_encoded_seqs'][idx].long(),
+      'token_start_positions': self.data['token_start_positions'][idx].float()
     }
 
 class LambdaLayer(nn.Module):
@@ -182,7 +192,7 @@ class TokStartAttn(nn.Module):
     self.emb = nn.Embedding(h['char_vocab_size'], h['char_embedding_size'])
 
     self.char_input_window_size = h['conv.kernel|filter_sizes'][0][0]
-    self.conv = ConvSegment(h['char_embedding_size'], h['conv_activation'], h['conv.kernel|filter_sizes'])
+    self.conv = ConvSegment(h['char_embedding_size'], h['conv_activation'], h['conv.kernel|filter_sizes'], True)
     conv_out_size = h['conv.kernel|filter_sizes'][-1][1]
 
     # no nonlinearity